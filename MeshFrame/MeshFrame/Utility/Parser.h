0f1f091db7c57773d126f1cbb742f0a721ac7011
Changyu Hu <1164636191@qq.com>
2024-12-13 02:33:11 +0000

    update

--- MeshFrame/MeshFrame/Utility/Parser.h
+++ MeshFrame/MeshFrame/Utility/Parser.h
@@ -7,46 +7,49 @@
 #include <iostream>
 #include <fstream>
 #include "../Json/json.hpp"
-#define REQUIRE_NUM_INPUTS(n)\
-if (argc < n+1) {\
-        std::cout << "[Error] Need at least " << n << " input parameters!" << std::endl; \
-        return -1;\
-}
+#define REQUIRE_NUM_INPUTS(n)                                                            \
+	if (argc < n + 1)                                                                    \
+	{                                                                                    \
+		std::cout << "[Error] Need at least " << n << " input parameters!" << std::endl; \
+		return -1;                                                                       \
+	}
 
-#define PARSE_VEC3_INDEXED_BY_ROUND_BRACKET(jsonParam, vec3)\
-std::array<double, 3> vec3##Arr;\
-if (MF::parseJsonParameters(jsonParam, #vec3, vec3##Arr))\
-{\
-	vec3(0) = vec3##Arr[0];\
-	vec3(1) = vec3##Arr[1];\
-	vec3(2) = vec3##Arr[2];\
-}
+#define PARSE_VEC3_INDEXED_BY_ROUND_BRACKET(jsonParam, vec3)  \
+	std::array<double, 3> vec3##Arr;                          \
+	if (MF::parseJsonParameters(jsonParam, #vec3, vec3##Arr)) \
+	{                                                         \
+		vec3(0) = vec3##Arr[0];                               \
+		vec3(1) = vec3##Arr[1];                               \
+		vec3(2) = vec3##Arr[2];                               \
+	}
 
-#define PARSE_VEC3_INDEXED_BY_SQUARE_BRACKET(jsonParam, vec3)\
-std::array<double, 3> vec3##Arr;\
-if (MF::parseJsonParameters(jsonParam, #vec3, vec3##Arr))\
-{\
-	vec3[0] = vec3##Arr[0];\
-	vec3[1] = vec3##Arr[1];\
-	vec3[2] = vec3##Arr[2];\
-}\
+#define PARSE_VEC3_INDEXED_BY_SQUARE_BRACKET(jsonParam, vec3) \
+	std::array<double, 3> vec3##Arr;                          \
+	if (MF::parseJsonParameters(jsonParam, #vec3, vec3##Arr)) \
+	{                                                         \
+		vec3[0] = vec3##Arr[0];                               \
+		vec3[1] = vec3##Arr[1];                               \
+		vec3[2] = vec3##Arr[2];                               \
+	}
 
-#define PUT_TO_JSON_VEC3_INDEXED_BY_ROUND_BRACKET(jsonParam, vec3)\
-std::array<double, 3> vec3##yArr;\
-vec3##yArr[0] = vec3(0);\
-vec3##yArr[1] = vec3(1);\
-vec3##yArr[2] = vec3(2);\
-jsonParam[#vec3] = vec3##yArr;
+#define PUT_TO_JSON_VEC3_INDEXED_BY_ROUND_BRACKET(jsonParam, vec3) \
+	std::array<double, 3> vec3##yArr;                              \
+	vec3##yArr[0] = vec3(0);                                       \
+	vec3##yArr[1] = vec3(1);                                       \
+	vec3##yArr[2] = vec3(2);                                       \
+	jsonParam[#vec3] = vec3##yArr;
 
-#define PUT_TO_JSON_VEC3_INDEXED_BY_SQUARE_BRACKET(jsonParam, vec3)\
-std::array<double, 3> vec3##yArr;\
-vec3##yArr[0] = vec3[0];\
-vec3##yArr[1] = vec3[1];\
-vec3##yArr[2] = vec3[2];\
-jsonParam[#vec3] = vec3##yArr;
+#define PUT_TO_JSON_VEC3_INDEXED_BY_SQUARE_BRACKET(jsonParam, vec3) \
+	std::array<double, 3> vec3##yArr;                               \
+	vec3##yArr[0] = vec3[0];                                        \
+	vec3##yArr[1] = vec3[1];                                        \
+	vec3##yArr[2] = vec3[2];                                        \
+	jsonParam[#vec3] = vec3##yArr;
 
-namespace MF {
-	inline bool loadJson(std::string  filePath, nlohmann::json& j) {
+namespace MF
+{
+	inline bool loadJson(std::string filePath, nlohmann::json &j)
+	{
 		std::ifstream ifs(filePath);
 		if (ifs.is_open())
 		{
@@ -55,7 +58,7 @@ namespace MF {
 				ifs >> j;
 				return true;
 			}
-			catch (nlohmann::json::exception& e)
+			catch (nlohmann::json::exception &e)
 			{
 				std::cout << e.what() << '\n';
 				return false;
@@ -66,10 +69,10 @@ namespace MF {
 			std::cout << "Fail to open: " << filePath << '\n';
 			return false;
 		}
-
 	}
 
-	inline bool saveJson(std::string  filePath, nlohmann::json& j, int indent = -1) {
+	inline bool saveJson(std::string filePath, nlohmann::json &j, int indent = -1)
+	{
 		std::ofstream ofs(filePath);
 		if (ofs.is_open())
 		{
@@ -78,7 +81,7 @@ namespace MF {
 				ofs << j.dump(indent) << std::endl;
 				return true;
 			}
-			catch (nlohmann::json::exception& e)
+			catch (nlohmann::json::exception &e)
 			{
 				std::cout << e.what() << '\n';
 				return false;
@@ -91,13 +94,14 @@ namespace MF {
 		}
 	}
 
-	inline nlohmann::json tryGetJson(nlohmann::json j, std::string  name) {
+	inline nlohmann::json tryGetJson(nlohmann::json j, std::string name)
+	{
 		nlohmann::json param;
 		try
 		{
 			param = j.at(name);
 		}
-		catch (nlohmann::json::exception& e)
+		catch (nlohmann::json::exception &e)
 		{
 			std::cout << e.what() << '\n';
 			std::cout << name << " does not exist in json and will be set to default value." << std::endl;
@@ -106,43 +110,47 @@ namespace MF {
 		return param;
 	}
 
-	template<typename T>
-	bool parseJsonParameters(nlohmann::json j, const std::string& name, T& param) {
+	template <typename T>
+	bool parseJsonParameters(nlohmann::json j, const std::string &name, T &param)
+	{
 		try
 		{
 			nlohmann::json paramJson = j.at(name);
 			param = paramJson.get<T>();
 		}
-		catch (nlohmann::json::exception& e)
+		catch (nlohmann::json::exception &e)
 		{
-			std::cout << "When loading: " << name << "\n"
-			    << e.what() << "\n"
-				<< name << " does not exist in json and will be set to default value." << std::endl;
+			// std::cout << "When loading: " << name << "\n"
+			//     << e.what() << "\n"
+			// 	<< name << " does not exist in json and will be set to default value." << std::endl;
 			return false;
 		}
 		return true;
 	}
 
-	template<typename T>
-	bool convertJsonParameters(nlohmann::json j, T& param) {
+	template <typename T>
+	bool convertJsonParameters(nlohmann::json j, T &param)
+	{
 		try
 		{
 			param = j.get<T>();
 		}
-		catch (nlohmann::json::exception& e)
+		catch (nlohmann::json::exception &e)
 		{
-			std::cout<< e.what() << "\n";
+			std::cout << e.what() << "\n";
 			return false;
 		}
 		return true;
 	}
 
-	struct BaseJsonConfig {
+	struct BaseJsonConfig
+	{
 
-		virtual bool fromJson(nlohmann::json& j) = 0;
-		virtual bool toJson(nlohmann::json& j) = 0;
+		virtual bool fromJson(nlohmann::json &j) = 0;
+		virtual bool toJson(nlohmann::json &j) = 0;
 
-		bool loadFromJsonFile(const std::string& inJsonParamFile, void * userData_in =nullptr) {
+		bool loadFromJsonFile(const std::string &inJsonParamFile, void *userData_in = nullptr)
+		{
 			nlohmann::json j;
 
 			if (userData_in != nullptr)
@@ -168,7 +176,8 @@ namespace MF {
 			}
 		}
 
-		bool writeToJsonFile(std::string filePath, int indent = -1, void* userData_in = nullptr) {
+		bool writeToJsonFile(std::string filePath, int indent = -1, void *userData_in = nullptr)
+		{
 			std::ofstream ofs(filePath);
 
 			if (userData_in != nullptr)
@@ -189,7 +198,7 @@ namespace MF {
 				return false;
 			}
 		}
-		void* userData = nullptr;
+		void *userData = nullptr;
 	};
 
 }
